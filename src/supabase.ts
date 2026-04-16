565c7445a6a40ac20aa4ec558030d12bae26ab89
Fabian Schütz <fabianschutz@Fabians-Mac-mini.local>
2026-04-16 19:08:01 +0200

    fix supabase key

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://mxtzdmsxlvcidshoqoax.supabase.co'
-const supabaseKey = sb_publishable_CzON1wflQJPN284Mmyzh4Q_6Y4Kwc18
+const supabaseKey = 'sb_publishable_CzON1wflQJPN284Mmyzh4Q_6Y4Kwc18
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
